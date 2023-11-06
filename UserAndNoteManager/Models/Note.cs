47260c29de20548bb30c55c69662a694ef8ca174
omidsn7 <omid.snnnn@gmail.com>
2023-11-07 00:12:29 +0330

    Add Data annotations For Models

--- UserAndNoteManager/Models/Note.cs
+++ UserAndNoteManager/Models/Note.cs
@@ -1,13 +1,26 @@
-﻿namespace UserAndNoteManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAndNoteManager.Models
 {
     public class Note
     {
+        [Key]
         public int ID { get; set; }
-        public string Content { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
-        public int Views { get; set; }
-        public bool Published { get; set; }
 
+        [Required]
+        [RegularExpression("^[a-zA-Z ]*$")]
+        public string Content { get; set; }
+        
+        [Required]
+        public DateTime DateCreated { get; set; }
+        
+        [Required]
+        public DateTime DateModified { get; set; }
+        
+        [Required]
+        public int Views { get; set; }
+        
+        [Required]
+        public bool Published { get; set; }
     }
 }
--- UserAndNoteManager/Models/User.cs
+++ UserAndNoteManager/Models/User.cs
@@ -1,12 +1,28 @@
-﻿namespace UserAndNoteManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAndNoteManager.Models
 {
     public class User
     {
+        [Key]
         public int ID { get; set; }
+
+        [Required]
+        [MaxLength(128)]
         public string FirsName { get; set; }
+
+        [MaxLength(128)]
         public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [MaxLength(128)]
         public int Age { get; set; }
+
         public string Website { get; set; }
 
     }
