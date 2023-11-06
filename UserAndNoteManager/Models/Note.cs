6c53e246ee7f85e17fd8062ed9b87280452248d6
omidsn7 <omid.snnnn@gmail.com>
2023-11-07 00:02:42 +0330

    Add User And Notes Models

---
+++ UserAndNoteManager/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿namespace UserAndNoteManager.Models
+{
+    public class Note
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int Views { get; set; }
+        public bool Published { get; set; }
+
+    }
+}
---
+++ UserAndNoteManager/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace UserAndNoteManager.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Website { get; set; }
+
+    }
+}
