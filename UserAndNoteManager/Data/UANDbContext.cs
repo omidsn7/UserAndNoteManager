6d6b80f7d2fc650a3d573339c1f7b24219af915f
omidsn7 <omid.snnnn@gmail.com>
2023-11-07 00:34:20 +0330

    Add On Model Creating For Seed And Add Connection String

---
+++ UserAndNoteManager/Data/UANDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAndNoteManager.Models;
+
+namespace UserAndNoteManager.Data
+{
+    public class UANDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+        public UANDbContext(DbContextOptions<UANDbContext> options) : base(options)
+        {
+               
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity =>
+
+            entity.HasData(new User
+            {
+                ID = 1,
+                FirsName = "Admin",
+                LastName = "Admin",
+                Age = 24,
+                Email = "Admin@Admin.Com",
+                Website = "www.Admin.com"
+            }));
+        }
+    }
+}
--- UserAndNoteManager/Program.cs
+++ UserAndNoteManager/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using UserAndNoteManager.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<UANDbContext>(Options => Options.UseSqlite(builder.Configuration.GetConnectionString("MyCon")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
